ec19bd721b50f6cc661d39a74a4a9b6b97ea91fc
RachaelKivuti <kivutingatha@gmail.com>
2022-03-12 11:47:26 +0000

    tasks

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the lowercase alphabet in reverse,
- * followed by a new line, using only putchar
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
