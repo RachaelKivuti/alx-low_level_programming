54e1bddcfcf5bd66e880e00954d5a1f1d2e51ad7
RachaelKivuti <kivutingatha@gmail.com>
2022-03-15 21:36:59 +0000

    task commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,4 +2,10 @@
 
 /**
  * _isalpha - check for lowercase letter
- * @c : character
+ * @c : character to check
+ * Return:0 or 1
+ */
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+}
