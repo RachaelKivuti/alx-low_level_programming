c014603ade70bbbd67d05340ec6a8eee52cfb015
RachaelKivuti <99282287+RachaelKivuti@users.noreply.github.com>
2022-03-16 05:40:37 +0300

    Update 104-fibonacci.c
    advanced task commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#define LARGEST 100000000000
+#define LARGEST 10000000000
 /**
  * main - main block
- * find and print the first 98 fib numbers starting either one and two
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ * Numbers should be coma and space separated.
  * Return: 0
  */
 int main(void)
