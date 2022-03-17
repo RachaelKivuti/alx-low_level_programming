8bcdee22278ad28ef48ac7c63744ee57f134d7e4
RachaelKivuti <kivutingatha@gmail.com>
2022-03-17 00:12:51 +0000

    task commit

--- 0x02-functions_nested_loops/100-main.c
+++
@@ -1,1 +0,0 @@
-typedef int make_betty_happy;
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * prints_times_table - prints time table
+ * print_times_table - print time table
  * @n : times table to use
  * Return:void
  */
-void prints_times_table(int n)
+void print_times_table(int n)
 {
-	int i = 0, rep, b;
+	int a = 0, rep, b;
 
 	if (n < 0 || n > 15)
 		return;
-	while (i <= n)
+	while (a <= n)
 	{
 		for (b = 0; b <= n; ++b)
 		{
-			rep = i * b;
+			rep = a * b;
 			if (b == 0)
 				_putchar('0' + rep);
 			else if (rep < 10)
@@ -42,6 +42,6 @@ void prints_times_table(int n)
 			}
 		}
 		_putchar('\n');
-		++i;
+		++a;
 	}
 }
