db2b5116603f64afab98c8848f6c52872c01f26d
RachaelKivuti <kivutingatha@gmail.com>
2022-03-21 18:48:52 +0000

    pointers and arrays task commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,8 @@
 /**
  * swap_int - Swaps the values of two integers.
  * @a: The first integer to be swapped.
- * @n: The second integer to be swapped.
+ * @b: The second integer to be swapped.
+ * Return: Nothing
  */
 void swap_int(int *a, int *b)
 {
