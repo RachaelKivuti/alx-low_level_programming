2f2d5254cfc09b7e10cddafdfb2b63b9ddaba844
RachaelKivuti <kivutingatha@gmail.com>
2022-03-30 15:59:45 +0000

    recursion task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,7 +17,7 @@ int sqrt_check(int g, int c)
 }
 
 /**
- * _sqrt_recusrion - return the natural square root of a number
+ * _sqrt_recursion - return the natural square root of a number
  * @n: integer to find sqrt of
  *
  * Return: natural square root or -1
