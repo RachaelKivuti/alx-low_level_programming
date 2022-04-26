188a126d6aabfad673662248d498edfaf4846feb
RachaelKivuti <kivutingatha@gmail.com>
2022-04-26 13:56:28 +0000

    task commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ *
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
+ */
+
 #include <stdlib.h>
 
 /**
@@ -29,3 +39,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
+#endif /* LISTS_H */
